fe6bf143d3e35f5d75eb06d896a6c99ac4aacd25
EdmondNeziraj <edmondneziraj10@gmail.com>
2022-12-03 18:12:05 -0500

    Added a basic initial match component

---
+++ src/components/Match.jsx
@@ -0,0 +1,16 @@
+import '../styles/Match.css';
+
+function Match() {
+    return (
+        <div className='card'>
+            <img src='#'></img>
+            <div className='card-body'>
+                <h4 className='card-title'> Title </h4>
+                <p>Time and Date</p>
+                <button>Join Game</button>
+            </div>
+        </div>
+    );
+}
+
+export default App;
