0f474954da439ce47842f707e4827c94fe390c15
EdmondNeziraj <edmondneziraj10@gmail.com>
2022-12-08 23:06:10 -0500

    Implemented basic CRUD infrastructure

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Match = require('./models/match');
 
 // connect the database to the server
@@ -20,9 +21,9 @@ db.once('open', () => {
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('method'));
 
 // routes
 app.get('/', (req, res) => {
@@ -34,7 +35,7 @@ app.get('/home', (req, res) => {
 })
 
 app.get('/matches', async (req, res) => {
-    let html = '<ul>';
+    let html = '<a href="/host">host a match</a><ul>';
     const matches = await Match.find({});
     for (let match of matches) {
         html += `<li><a href="/matches/${match.id}">${match.location}</a></li>`
@@ -43,7 +44,7 @@ app.get('/matches', async (req, res) => {
     res.send(html);
 })
 
-app.get('/matches/new', (req, res) => {
+app.get('/host', (req, res) => {
     console.log("hello from new match form")
     res.send(`
         <div className="App">
@@ -62,6 +63,7 @@ app.get('/matches/new', (req, res) => {
                     </div>
                     <button>Create Match</button>
                 </form>
+                <a href="/matches">all matches</a>
             </div>
     `)
 })
@@ -76,14 +78,50 @@ app.post('/matches', async (req, res) => {
 
 app.get('/matches/:id', async (req, res) => {
     const match = await Match.findById(req.params.id)
-    res.send(`details of match <b>${match.location}</b> with id <b>${req.params.id}</b>`);
+    res.send(`
+    <h1>view</h1>
+    <div>details of match <b>${match.location}</b> with id <b>${req.params.id}</b></div>
+    <form action="/matches/${match._id}?method=DELETE" method="POST"><button>Delete</button></form>
+    <a href="/matches/${match._id}/edit">edit match</a>
+    <div><a href="/matches">all matches </a></div>`);
 })
 
-app.get('/host', (req, res) => {
-    res.send('hello from host')
+app.get('/matches/:id/edit', async (req, res) => {
+    const match = await Match.findById(req.params.id)
+    res.send(`
+        <div className="App">
+            <h1>edit</h1>
+            <form action="/matches/${match._id}?method=PUT" method="POST">
+                <div>
+                    <label>
+                        Location
+                        <input type="text" name="match[location]" value="${match.location}"/>
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Time
+                        <input type="text" name="match[time]" value="${match.time}"/>
+                    </label>
+                </div>
+                <button>Update Match</button>
+            </form>
+            <a href="/matches/${match._id}">back to match</a>
+        </div>
+    `)
 })
 
+app.put('/matches/:id', async (req, res) => {
+    const { id } = req.params;
+    const match = await Match.findByIdAndUpdate(id, { ...req.body.match }); 
+    res.redirect(`/matches/${match._id}`);
+})
 
+app.delete('/matches/:id', async (req, res) => {
+    const { id } = req.params;
+    await Match.findByIdAndDelete(id);
+    res.redirect('/matches');
+})
 
 const port = 5000;
 app.listen(port, () => {
