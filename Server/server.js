71a5728797b99f46bb772ed62a31295b0aaee98f
EdmondNeziraj <edmondneziraj10@gmail.com>
2022-12-08 22:30:55 -0500

    Developed additional endpoints

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ const Match = require('./models/match');
 // connect the database to the server
 mongoose.connect('mongodb://localhost:27017/4-3-3', {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 })
 
 const db = mongoose.connection;
@@ -25,29 +25,58 @@ app.use(express.json());
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
+app.get('/', (req, res) => {
+    res.send('hello from home');
+})
+
 app.get('/home', (req, res) => {
     res.send('hello from home');
 })
 
 app.get('/matches', async (req, res) => {
+    let html = '<ul>';
     const matches = await Match.find({});
-    res.send(matches);
+    for (let match of matches) {
+        html += `<li><a href="/matches/${match.id}">${match.location}</a></li>`
+    }
+    html += '</ul>';
+    res.send(html);
 })
 
 app.get('/matches/new', (req, res) => {
     console.log("hello from new match form")
-    res.send("hello from new match form")
+    res.send(`
+        <div className="App">
+                <form action="/matches" method="POST">
+                    <div>
+                        <label>
+                            Location
+                            <input type="text" name="match[location]"/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Time
+                            <input type="text" name="match[time]"/>
+                        </label>
+                    </div>
+                    <button>Create Match</button>
+                </form>
+            </div>
+    `)
 })
 
-app.post('matches', async (req, res) => {
-    console.log(req.body)
-    res.send(req.body);
+app.post('/matches', async (req, res) => {
+    console.log(req.body.match)
+    const match = new Match(req.body.match);
+    await match.save();
+    console.log(`save match ${match} in db`);
+    res.redirect(`/matches/${match._id}`);
 })
 
 app.get('/matches/:id', async (req, res) => {
-    console.log(req.params.id);
-    const match = await Match.findById(req.params.id);
-    res.send(match)
+    const match = await Match.findById(req.params.id)
+    res.send(`details of match <b>${match.location}</b> with id <b>${req.params.id}</b>`);
 })
 
 app.get('/host', (req, res) => {
